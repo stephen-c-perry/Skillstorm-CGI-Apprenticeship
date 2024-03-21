ea32062602123ba7922e1feb855db9edfb73d280
stephen-c-perry <stephen.christian.perry@gmail.com>
2024-03-21 16:45:12 -0500

    runs but slight bug

--- week2/day4/day4/src/DailyAssignment.java
+++ week2/day4/day4/src/DailyAssignment.java
@@ -10,10 +10,15 @@ public class DailyAssignment {
          * An array with more than one element
          */
 
-        int[] multi = { 1, 2, 3, 4, 5 };
-        int[] single = { 1 };
+        int[] multi = { 10, 20, 30, 40, 50 };
+        int[] single = { 77 };
         int[] empty = {};
         int[] nulls = null;
+
+        int multiMax = findMax(multi);
+        int singleMax = findMax(single);
+        int emptyMax = findMax(empty);
+        int nullsMax = findMax(nulls);
 
     }
 
@@ -21,14 +26,21 @@ public class DailyAssignment {
     public static int findMax(int[] arr) {
         int maxValue = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= maxValue) {
-                maxValue = arr[i];
-                System.out.println("The max value is: " + maxValue);
-            }
+        if (arr == null || arr.length == 0) {
+            System.out.println("Array is null or empty.  Try again.");
+        } else
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > maxValue) {
+                    // bug: prints each value in multiple element array
+                    // only want the element with the highest value
+                    maxValue = arr[i];
+                    System.out.println("The max value is: " + maxValue);
+                    // return maxValue;
+                }
 
-        }
+            }
         return maxValue;
+
     }
 
 }
