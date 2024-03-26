dab788d5655170cf921970cf5856cbed396e5092
stephen-c-perry <stephen.christian.perry@gmail.com>
2024-03-26 09:26:30 -0500

    OOP practice

---
+++ week3/day2/src/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    public String name = "no name";
+    public String hairColor = "no color";
+    public Boolean spayedOrNeutered = false;
+    public double weight = 0.0;
+    public String owner = "no owner";
+
+    public Cat(String name, String hairColor, Boolean spayedOrNeutered, double weight, String owner){
+        this.name = name;
+        this.hairColor = hairColor;
+        this.spayedOrNeutered = spayedOrNeutered;
+        this.weight = weight;
+        this.owner = owner;
+    }
+}
