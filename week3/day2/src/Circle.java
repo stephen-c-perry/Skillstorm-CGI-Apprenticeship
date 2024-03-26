05768fa573747caf40e550c7e41e00ff0eda4683
stephen-c-perry <stephen.christian.perry@gmail.com>
2024-03-26 16:43:29 -0500

    renamed directories

---
+++ week3/day2/src/Circle.java
@@ -0,0 +1,60 @@
+public class Circle {
+
+    /*
+     * Step 1. Create a class, Circle, that describes the properties and
+     * calculations for working with circles. These circles should store pertinent
+     * data and be able to perform operations such as calculating the diameter,
+     * circumference, and area.
+     */
+
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius,2);
+    }
+
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.Pi * radius;
+    }
+
+
+    
+    /* Step 2. In a separate class, create a main method. Instantiate 2 circle
+     * objects and print out the details about each circle. Change the size or color
+     * or some other property of one of the circles and then print them out again.
+     * (Note, the other circles' values should not have changed).
+     * 
+     * Requirements:
+     * 
+     * Contains useful methods calculating a circle's diameter, circumference, and
+     * area.
+     * Be able to create a Circle object by either providing a radius, or, if none
+     * is provided, creates a circle with some default radius instead.
+     * Have the ability to create multiple Circle objects and have their data be
+     * independent of one another.
+     * Fully encapsulate the class
+     * Bonus Challenge:
+     * 
+     * Create a method that compares one Circle object with another and determine
+     * whether they're equal or not.
+     * Push your code to GitHub and provide a link instead of uploading your .java
+     * file.
+     */
+
+}
---
+++ week3/day2/src/CircleTest.java
@@ -0,0 +1,12 @@
+public class CircleTest {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Step 2. In a separate class, create a main method. Instantiate 2 circle
+         * objects and print out the details about each circle. Change the size or color
+         * or some other property of one of the circles and then print them out again.
+         * (Note, the other circles' values should not have changed).
+         */
+
+
+    }
+}
