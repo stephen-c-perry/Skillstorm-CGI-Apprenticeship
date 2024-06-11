589a54dfb3d3fcb4b180ba450e857a08d0f2a105
stephen-c-perry <stephen.christian.perry@gmail.com>
2024-06-11 16:59:21 -0500

    .

--- week14/day-2/src/app/models/HouseItem.ts
+++
@@ -1,8 +0,0 @@
-export class HouseItem {
-    constructor(
-      public name: string,
-      public quantity: number,
-      public category: string
-    ) {}
-  }
-  
---
+++ week14/day-2/src/app/models/house-things.ts
@@ -0,0 +1,18 @@
+[
+    {
+        "toy": "Whiskers",
+        "owner": "Siamese"
+    },
+    {
+        "toy": "Fluffy",
+        "owner": "Persian"
+    },
+    {
+        "toy": "Shadow",
+        "owner": "Maine Coon"
+    },
+    {
+        "toy": "Mittens",
+        "owner": "British Shorthair"
+    }
+]
